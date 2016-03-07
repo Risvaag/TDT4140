696e6c570a2cfe4c4ca4464f0fa75d378ba18414
nornes <andrehn@stud.ntnu.no>
2016-03-07 14:31:36 +0100

    draws a map with your current location centered

---
+++ htmlÄndMore/app.js
@@ -0,0 +1,25 @@
+
+
+var positionToGoogleLatLng = function(position) {
+  return new google.maps.LatLng(
+    position.coords.latitude,
+    position.coords.longitude
+  );
+};
+
+var drawStoresMap = function(position) {
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: positionToGoogleLatLng(position),
+    zoom: 12
+  });
+};
+
+$(document).ready(function() {
+    navigator.geolocation.getCurrentPosition( 
+        function(position) {
+            console.log(position);
+            drawStoresMap(position) }, 
+        function(error) {
+            console.log(error) }
+        );
+  });
